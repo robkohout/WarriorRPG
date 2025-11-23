bbd714a79a38fa12e13904d4d3ba076a531f19bf
robkohout <robkohout@hotmail.com>
2025-11-22 18:58:09 -0800

    Calculate Final Damage Done

--- Source/Warrior/Private/AbilitySystem/GEExecCalc/GEExecCalc_DamageTaken.cpp
+++ Source/Warrior/Private/AbilitySystem/GEExecCalc/GEExecCalc_DamageTaken.cpp
@@ -4,16 +4,19 @@
 #include "AbilitySystem/GEExecCalc/GEExecCalc_DamageTaken.h"
 #include "WarriorGameplayTags.h"
 #include "AbilitySystem/WarriorAttributeSet.h"
+#include "WarriorDebugHelper.h"
 
 struct FWarriorDamageCapture
 {
 	DECLARE_ATTRIBUTE_CAPTUREDEF(AttackPower);
 	DECLARE_ATTRIBUTE_CAPTUREDEF(DefensePower);
+	DECLARE_ATTRIBUTE_CAPTUREDEF(DamageTaken);
 	
 	FWarriorDamageCapture()
 	{
 		DEFINE_ATTRIBUTE_CAPTUREDEF(UWarriorAttributeSet, AttackPower, Source, false);
 		DEFINE_ATTRIBUTE_CAPTUREDEF(UWarriorAttributeSet, DefensePower, Target, false);
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UWarriorAttributeSet, DamageTaken, Target, false);
 	}
 };
 
@@ -40,6 +43,7 @@ UGEExecCalc_DamageTaken::UGEExecCalc_DamageTaken()
 	
 	RelevantAttributesToCapture.Add(GetWarriorDamageCapture().AttackPowerDef);
 	RelevantAttributesToCapture.Add(GetWarriorDamageCapture().DefensePowerDef);
+	RelevantAttributesToCapture.Add(GetWarriorDamageCapture().DamageTakenDef);
 }
 
 void UGEExecCalc_DamageTaken::Execute_Implementation(
@@ -59,6 +63,7 @@ void UGEExecCalc_DamageTaken::Execute_Implementation(
 	
 	float SourceAttackPower = 0.f;
 	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(GetWarriorDamageCapture().AttackPowerDef, EvaluateParams, SourceAttackPower);
+	Debug::Print(TEXT("SourceAttackPower"), SourceAttackPower);
 	
 	float BaseDamage = 0.f;
 	int32 UsedLightAttackComboCount = 0;
@@ -69,19 +74,49 @@ void UGEExecCalc_DamageTaken::Execute_Implementation(
 		if (TagMagnitude.Key.MatchesTagExact(WarriorGameplayTags::Shared_SetByCaller_BaseDamage))
 		{
 			BaseDamage = TagMagnitude.Value;
+			Debug::Print(TEXT("BaseDamage"), BaseDamage);
 		}
 		
 		if (TagMagnitude.Key.MatchesTagExact(WarriorGameplayTags::Player_SetByCaller_AttackType_Light))
 		{
 			UsedLightAttackComboCount = TagMagnitude.Value;		
+			Debug::Print(TEXT("UsedLightAttackComboCount"), UsedLightAttackComboCount);
 		}
 		
 		if (TagMagnitude.Key.MatchesTagExact(WarriorGameplayTags::Player_SetByCaller_AttackType_Heavy))
 		{
-			UsedHeavyAttackComboCount = TagMagnitude.Value;		
+			UsedHeavyAttackComboCount = TagMagnitude.Value;	
+			Debug::Print(TEXT("UsedHeavyAttackComboCount"), UsedHeavyAttackComboCount);
 		}
 	}
 	
 	float TargetDefensePower = 0.f;
 	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(GetWarriorDamageCapture().DefensePowerDef, EvaluateParams, TargetDefensePower);
+	Debug::Print(TEXT("TargetDefensePower"), TargetDefensePower);
+	
+	if (UsedLightAttackComboCount != 0)
+	{
+		const float DamageIncreasePercentLight = (UsedLightAttackComboCount - 1) * 0.05f + 1.f;
+		BaseDamage *= DamageIncreasePercentLight;
+		Debug::Print(TEXT("ScaledBaseDamageLight"), BaseDamage);
+	}
+	
+	if (UsedHeavyAttackComboCount != 0)
+	{
+		const float DamageIncreasePercentHeavy = UsedHeavyAttackComboCount * 0.15f + 1.f;
+		BaseDamage *= DamageIncreasePercentHeavy;
+		Debug::Print(TEXT("ScaledBaseDamageHeavy"), BaseDamage);
+	}
+	
+	const float FinalDamageDone = BaseDamage * SourceAttackPower / TargetDefensePower;
+	Debug::Print(TEXT("FinalDamageDone"), FinalDamageDone);
+	
+	if (FinalDamageDone > 0.f)
+	{
+		OutExecutionOutput.AddOutputModifier(
+			FGameplayModifierEvaluatedData(
+				GetWarriorDamageCapture().DamageTakenProperty,
+				EGameplayModOp::Override,
+				FinalDamageDone));
+	}
 }
--- Source/Warrior/Public/WarriorDebugHelper.h
+++ Source/Warrior/Public/WarriorDebugHelper.h
@@ -12,4 +12,14 @@ namespace Debug
 		}
 		UE_LOG(LogTemp, Warning, TEXT("%s"), *Message);
 	}
+	
+	static void Print(const FString& FloatTitle, float FloatValueToPrint, int32 InKey = -1, const FColor& Color = FColor::MakeRandomColor())
+	{
+		const FString FinalMessage = FloatTitle + TEXT(": ") + FString::SanitizeFloat(FloatValueToPrint);
+		if (GEngine)
+		{
+			GEngine->AddOnScreenDebugMessage(InKey, 7.f, Color, FinalMessage);
+		}
+		UE_LOG(LogTemp, Warning, TEXT("%s"), *FinalMessage);
+	}
 }
