6cf5e0fa0fc30056deb491fd70f29a47b90d67eb
robkohout <robkohout@hotmail.com>
2025-11-25 00:13:32 -0800

    AI Perception

--- Source/Warrior/Private/Controllers/WarriorAIController.cpp
+++ Source/Warrior/Private/Controllers/WarriorAIController.cpp
@@ -4,6 +4,8 @@
 #include "Controllers/WarriorAIController.h"
 #include "Navigation/CrowdFollowingComponent.h"
 #include "WarriorDebugHelper.h"
+#include "Perception/AIPerceptionComponent.h"
+#include "Perception/AISenseConfig_Sight.h"
 
 
 AWarriorAIController::AWarriorAIController(const FObjectInitializer& ObjectInitializer)
@@ -13,4 +15,21 @@ AWarriorAIController::AWarriorAIController(const FObjectInitializer& ObjectIniti
 	{
 		Debug::Print(TEXT("CrowdFollowingComponent valid"), FColor::Green);
 	}
+	
+	EnemySenseConfig_Sight = CreateDefaultSubobject<UAISenseConfig_Sight>(TEXT("EnemySenseConfig_Sight"));
+	EnemySenseConfig_Sight->DetectionByAffiliation.bDetectEnemies = true;
+	EnemySenseConfig_Sight->DetectionByAffiliation.bDetectFriendlies = false;
+	EnemySenseConfig_Sight->DetectionByAffiliation.bDetectNeutrals = false;
+	EnemySenseConfig_Sight->SightRadius = 5000.f;
+	EnemySenseConfig_Sight->LoseSightRadius = 0.f;
+	EnemySenseConfig_Sight->PeripheralVisionAngleDegrees = 360.f;
+	
+	EnemyPerceptionComponent = CreateDefaultSubobject<UAIPerceptionComponent>(TEXT("EnemyPerceptionComponent"));
+	EnemyPerceptionComponent->ConfigureSense(*EnemySenseConfig_Sight);
+	EnemyPerceptionComponent->SetDominantSense(UAISenseConfig_Sight::StaticClass());
+	EnemyPerceptionComponent->OnTargetPerceptionUpdated.AddUniqueDynamic(this, &ThisClass::OnEnemyPerceptionUpdated);
+}
+
+void AWarriorAIController::OnEnemyPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus)
+{
 }
--- Source/Warrior/Public/Controllers/WarriorAIController.h
+++ Source/Warrior/Public/Controllers/WarriorAIController.h
@@ -6,6 +6,9 @@
 #include "Runtime/AIModule/Classes/AIController.h"
 #include "WarriorAIController.generated.h"
 
+struct FAIStimulus;
+class UAISenseConfig_Sight;
+
 UCLASS()
 class WARRIOR_API AWarriorAIController : public AAIController
 {
@@ -13,4 +16,14 @@ class WARRIOR_API AWarriorAIController : public AAIController
 	
 public:
 	AWarriorAIController(const FObjectInitializer& ObjectInitializer);
+	
+protected:
+	UFUNCTION()
+	virtual void OnEnemyPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus);
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	UAIPerceptionComponent* EnemyPerceptionComponent;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	UAISenseConfig_Sight* EnemySenseConfig_Sight;
 };
