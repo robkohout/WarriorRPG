5735757cb6b2e3fa6d869bd2caadce2109d88fb1
robkohout <robkohout@hotmail.com>
2025-11-25 15:52:44 -0800

    Orient To Target Actor

---
+++ Source/Warrior/Private/AI/BTService_OrientToTargetActor.cpp
@@ -0,0 +1,55 @@
+﻿// Rob Kohout All Rights Reserved
+
+
+#include "AI/BTService_OrientToTargetActor.h"
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "BehaviorTree/BlackboardData.h"
+#include "Kismet/KismetMathLibrary.h"
+
+UBTService_OrientToTargetActor::UBTService_OrientToTargetActor()
+{
+	NodeName = TEXT("Native Orient Rotation To Target Actor");
+	
+	INIT_SERVICE_NODE_NOTIFY_FLAGS();
+	
+	RotationInterpSpeed = 5.f;
+	Interval = 0.f;
+	RandomDeviation = 0.f;
+	
+	InTargetActorKey.AddObjectFilter(this, GET_MEMBER_NAME_CHECKED(ThisClass, InTargetActorKey), AActor::StaticClass());
+}
+
+void UBTService_OrientToTargetActor::InitializeFromAsset(UBehaviorTree& Asset)
+{
+	Super::InitializeFromAsset(Asset);
+	
+	if (UBlackboardData* BBAsset = GetBlackboardAsset())
+	{
+		InTargetActorKey.ResolveSelectedKey(*BBAsset);
+	}
+}
+
+FString UBTService_OrientToTargetActor::GetStaticDescription() const
+{
+	const FString KeyDescription = InTargetActorKey.SelectedKeyName.ToString();
+
+	return FString::Printf(TEXT("Orient rotation to %s Key %s"),*KeyDescription,*GetStaticServiceDescription());
+}
+
+void UBTService_OrientToTargetActor::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+	
+	UObject* ActorObject = OwnerComp.GetBlackboardComponent()->GetValueAsObject(InTargetActorKey.SelectedKeyName);
+	const AActor* TargetActor = Cast<AActor>(ActorObject);
+	APawn* OwningPawn = OwnerComp.GetAIOwner()->GetPawn();
+	
+	if (OwningPawn && TargetActor)
+	{
+		const FRotator LookAtRotation = UKismetMathLibrary::FindLookAtRotation(OwningPawn->GetActorLocation(), TargetActor->GetActorLocation());
+		const FRotator TargetRotation = UKismetMathLibrary::RInterpTo(OwningPawn->GetActorRotation(), LookAtRotation, DeltaSeconds, RotationInterpSpeed);
+		
+		OwningPawn->SetActorRotation(TargetRotation);
+	}
+}
