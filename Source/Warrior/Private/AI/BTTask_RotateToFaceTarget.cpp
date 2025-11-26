8c7c096bc705b8f9e08119a6cb5bdfc03ef70b52
robkohout <robkohout@hotmail.com>
2025-11-26 00:18:15 -0800

    Rotate Enemy In Task

--- Source/Warrior/Private/AI/BTTask_RotateToFaceTarget.cpp
+++ Source/Warrior/Private/AI/BTTask_RotateToFaceTarget.cpp
@@ -2,7 +2,10 @@
 
 
 #include "AI/BTTask_RotateToFaceTarget.h"
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
 #include "BehaviorTree/BlackboardData.h"
+#include "Kismet/KismetMathLibrary.h"
 
 UBTTask_RotateToFaceTarget::UBTTask_RotateToFaceTarget()
 {
@@ -40,4 +43,71 @@ FString UBTTask_RotateToFaceTarget::GetStaticDescription() const
 	
 	return FString::Printf(TEXT("Smoothly rotates to face %s Key until the angle precision %s is reached"),
 		*KeyDescription, *FString::SanitizeFloat(AnglePrecision));
+}
+
+EBTNodeResult::Type UBTTask_RotateToFaceTarget::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	UObject* ActorObject = OwnerComp.GetBlackboardComponent()->GetValueAsObject(InTargetToFaceKey.SelectedKeyName);
+	AActor* TargetActor = Cast<AActor>(ActorObject);
+	APawn* OwningPawn = OwnerComp.GetAIOwner()->GetPawn();
+	
+	FRotateToFaceTargetTaskMemory* Memory = CastInstanceNodeMemory<FRotateToFaceTargetTaskMemory>(NodeMemory);
+	check(Memory);
+	
+	Memory->OwningPawn = OwningPawn;
+	Memory->TargetActor = TargetActor;
+	
+	if (!Memory->IsValid())
+	{
+		return EBTNodeResult::Failed;
+	}
+	
+	if (HasReachedAnglePrecision(OwningPawn, TargetActor))
+	{
+		Memory->Reset();
+		return EBTNodeResult::Succeeded;
+	}
+	
+	return EBTNodeResult::InProgress;
+}
+
+void UBTTask_RotateToFaceTarget::TickTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	FRotateToFaceTargetTaskMemory* Memory = CastInstanceNodeMemory<FRotateToFaceTargetTaskMemory>(NodeMemory);
+	
+	if (!Memory->IsValid())
+	{
+		FinishLatentTask(OwnerComp, EBTNodeResult::Failed);
+	}
+	
+	if (HasReachedAnglePrecision(Memory->OwningPawn.Get(), Memory->TargetActor.Get()))
+	{
+		Memory->Reset();
+		FinishLatentTask(OwnerComp, EBTNodeResult::Succeeded);
+	}
+	else
+	{
+		const FRotator LookAtRotation = UKismetMathLibrary::FindLookAtRotation(
+			Memory->OwningPawn->GetActorLocation(), 
+			Memory->TargetActor->GetActorLocation());
+		
+		const FRotator TargetRotation = UKismetMathLibrary::RInterpTo(
+			Memory->OwningPawn->GetActorRotation(), 
+			LookAtRotation,
+			DeltaSeconds,
+			RotationInterpSpeed);
+		
+		Memory->OwningPawn->SetActorRotation(TargetRotation);			
+	}
+}
+
+bool UBTTask_RotateToFaceTarget::HasReachedAnglePrecision(APawn* QueryPawn, AActor* TargetActor) const
+{
+	const FVector OwnerForward = QueryPawn->GetActorForwardVector();
+	const FVector OwnerToTargetNormalized = (TargetActor->GetActorLocation() - QueryPawn->GetActorLocation()).GetSafeNormal();
+	
+	const float DotResult = FVector::DotProduct(OwnerForward, OwnerToTargetNormalized);
+	const float AngleDiff = UKismetMathLibrary::DegAcos(DotResult);
+	
+	return AngleDiff <= AnglePrecision;
 }
