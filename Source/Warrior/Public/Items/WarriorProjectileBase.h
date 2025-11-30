7c5d094fd6b9c48c3158fdf3bbf9d29c2a09195e
robkohout <robkohout@hotmail.com>
2025-11-29 22:51:12 -0800

    On Projectile HIt

--- Source/Warrior/Public/Items/WarriorProjectileBase.h
+++ Source/Warrior/Public/Items/WarriorProjectileBase.h
@@ -28,6 +28,23 @@ public:
 protected:
 	virtual void BeginPlay() override;
 	
+	UFUNCTION()
+	virtual void OnProjectileHit(
+		UPrimitiveComponent* HitComponent, 
+		AActor* OtherActor, 
+		UPrimitiveComponent* OtherComp, 
+		FVector NormalImpulse, 
+		const FHitResult& Hit);
+	
+	UFUNCTION()
+	virtual void OnProjectileBeginOverlap(
+		UPrimitiveComponent* OverlappedComponent, 
+		AActor* OtherActor, 
+		UPrimitiveComponent* OtherComp, 
+		int32 OtherBodyIndex, 
+		bool bFromSweep, 
+		const FHitResult& SweepResult);
+	
 	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "Projectile")
 	UBoxComponent* ProjectileCollisionBox;
 	
@@ -38,5 +55,5 @@ protected:
 	UProjectileMovementComponent* ProjectileMovementComponent;
 	
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Projectile")
-	EProjectileDamagePolicy DamagePolicy = EProjectileDamagePolicy::OnHit;
+	EProjectileDamagePolicy ProjectileDamagePolicy  = EProjectileDamagePolicy::OnHit;
 };
