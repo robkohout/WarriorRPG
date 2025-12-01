00f75f95a9d6c983ff710078c9b94e2b815f01b1
robkohout <robkohout@hotmail.com>
2025-11-30 19:31:05 -0800

    Remove Boss Bar

--- Source/Warrior/Public/Components/UI/EnemyUIComponent.h
+++ Source/Warrior/Public/Components/UI/EnemyUIComponent.h
@@ -7,9 +7,20 @@
 #include "EnemyUIComponent.generated.h"
 
 
+class UWarriorWidgetBase;
+
 UCLASS()
 class WARRIOR_API UEnemyUIComponent : public UPawnUIComponent
 {
 	GENERATED_BODY()
 	
+public:
+	UFUNCTION(BlueprintCallable)
+	void RegisterEnemyDrawnWidget(UWarriorWidgetBase* InWidgetToRegister);
+	
+	UFUNCTION(BlueprintCallable)
+	void RemoveEnemyDrawnWidgetsIfAny();
+	
+private:
+	TArray<UWarriorWidgetBase*> EnemyDrawnWidgets;
 };
