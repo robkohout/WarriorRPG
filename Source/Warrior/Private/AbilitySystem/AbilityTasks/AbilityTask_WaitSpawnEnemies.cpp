3ea1e9e3737009f1dfd78734abe76d6edcf0aa45
robkohout <robkohout@hotmail.com>
2025-12-01 20:57:01 -0800

    Wait Spawn Enemies

---
+++ Source/Warrior/Private/AbilitySystem/AbilityTasks/AbilityTask_WaitSpawnEnemies.cpp
@@ -0,0 +1,24 @@
+﻿// Rob Kohout All Rights Reserved
+
+
+#include "AbilitySystem/AbilityTasks/AbilityTask_WaitSpawnEnemies.h"
+
+UAbilityTask_WaitSpawnEnemies* UAbilityTask_WaitSpawnEnemies::WaitSpawnEnemies(
+	UGameplayAbility* OwningAbility,
+	FGameplayTag EventTag, 
+	TSoftClassPtr<AWarriorEnemyCharacter> SoftEnemyClassToSpawn, 
+	int32 NumToSpawn,
+	const FVector& SpawnOrigin, 
+	float RandomSpawnRadius, 
+	const FRotator& SpawnRotation)
+{
+	UAbilityTask_WaitSpawnEnemies* Node = NewAbilityTask<UAbilityTask_WaitSpawnEnemies>(OwningAbility);
+	Node->CachedEventTag = EventTag;
+	Node->CachedSoftEnemyClassToSpawn = SoftEnemyClassToSpawn;
+	Node->CachedNumToSpawn = NumToSpawn;
+	Node->CachedSpawnOrigin = SpawnOrigin;
+	Node->CachedRandomSpawnRadius = RandomSpawnRadius;
+	Node->CachedSpawnRotation = SpawnRotation;
+	
+	return Node;
+}
