a8a6ce2dc5834e29d17f62687e0fad3a766f8c00
robkohout <robkohout@hotmail.com>
2025-12-07 00:02:23 -0800

    On Ability Icon Updated

--- Source/Warrior/Public/Components/UI/HeroUIComponent.h
+++ Source/Warrior/Public/Components/UI/HeroUIComponent.h
@@ -3,10 +3,12 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
 #include "PawnUIComponent.h"
 #include "HeroUIComponent.generated.h"
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnEquippedWeaponChangedDelegate, TSoftObjectPtr<UTexture2D>, SoftWeaponIcon);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnAbilityIconSlotUpdatedDelegate, FGameplayTag, AbilityInputTag, TSoftObjectPtr<UMaterialInterface>, SoftAbilityIconMaterial);
 
 UCLASS()
 class WARRIOR_API UHeroUIComponent : public UPawnUIComponent
@@ -19,4 +21,7 @@ public:
 	
 	UPROPERTY(BlueprintCallable, BlueprintAssignable)
 	FOnEquippedWeaponChangedDelegate OnEquippedWeaponChanged;
+	
+	UPROPERTY(BlueprintCallable, BlueprintAssignable)
+	FOnAbilityIconSlotUpdatedDelegate OnAbilityIconSlotUpdated;
 };
