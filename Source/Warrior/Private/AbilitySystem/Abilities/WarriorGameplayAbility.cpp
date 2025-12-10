a217f0fe426895667ffb30482d972dff40dd25f7
robkohout <robkohout@hotmail.com>
2025-12-09 22:41:52 -0800

    Heavy Ability Damage

--- Source/Warrior/Private/AbilitySystem/Abilities/WarriorGameplayAbility.cpp
+++ Source/Warrior/Private/AbilitySystem/Abilities/WarriorGameplayAbility.cpp
@@ -4,6 +4,8 @@
 #include "AbilitySystem/Abilities/WarriorGameplayAbility.h"
 #include "AbilitySystemBlueprintLibrary.h"
 #include "AbilitySystemComponent.h"
+#include "WarriorFunctionLibrary.h"
+#include "WarriorGameplayTags.h"
 #include "AbilitySystem/WarriorAbilitySystemComponent.h"
 #include "Components/Combat/PawnCombatComponent.h"
 
@@ -51,6 +53,36 @@ FActiveGameplayEffectHandle UWarriorGameplayAbility::BP_ApplyEffectSpecHandleToT
 	OutSuccessType = ActiveGameplayEffectHandle.WasSuccessfullyApplied() ? EWarriorSuccessType::Successful : EWarriorSuccessType::Failed;
 	
 	return ActiveGameplayEffectHandle;
+}
+
+void UWarriorGameplayAbility::ApplyGameplayEffectSpecHandleToHitResults(const FGameplayEffectSpecHandle& InSpecHandle, const TArray<FHitResult>& InHitResults)
+{
+	if (InHitResults.IsEmpty()) return;
+	
+	APawn* OwningPawn = CastChecked<APawn>(GetAvatarActorFromActorInfo());
+	
+	for (const FHitResult& Hit : InHitResults)
+	{
+		if (APawn* HitPawn = Cast<APawn>(Hit.GetActor()))
+		{
+			if (UWarriorFunctionLibrary::IsTargetPawnHostile(OwningPawn, HitPawn))
+			{
+				FActiveGameplayEffectHandle ActiveGameplayEffectHandle = NativeApplyEffectSpecHandleToTarget(HitPawn, InSpecHandle);
+				
+				if (ActiveGameplayEffectHandle.WasSuccessfullyApplied())
+				{
+					FGameplayEventData Data;
+					Data.Instigator = OwningPawn;
+					Data.Target = HitPawn;
+					
+					UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(
+						HitPawn, 
+						WarriorGameplayTags::Shared_Event_HitReact, 
+						Data);
+				}
+			}
+		}
+	}
 }
 
 UPawnCombatComponent* UWarriorGameplayAbility::GetPawnCombatComponentFromActorInfo() const
--- Source/Warrior/Public/AbilitySystem/Abilities/WarriorGameplayAbility.h
+++ Source/Warrior/Public/AbilitySystem/Abilities/WarriorGameplayAbility.h
@@ -37,6 +37,9 @@ protected:
 	UFUNCTION(BlueprintCallable, Category = "Warrior | Ability", meta=(DisplayName = "Apply Gameplay Effect Spec Handle To Target Actor", ExpandEnumAsExecs = "OutSuccessType"))
 	FActiveGameplayEffectHandle BP_ApplyEffectSpecHandleToTarget(AActor* TargetActor, const FGameplayEffectSpecHandle& InSpecHandle, EWarriorSuccessType& OutSuccessType);
 	
+	UFUNCTION(BlueprintCallable, Category = "Warrior | Ability")
+	void ApplyGameplayEffectSpecHandleToHitResults(const FGameplayEffectSpecHandle& InSpecHandle, const TArray<FHitResult>& InHitResults);
+	
 	UFUNCTION(BlueprintPure, Category = "Warrior | Ability")
 	UPawnCombatComponent* GetPawnCombatComponentFromActorInfo() const;
 	
