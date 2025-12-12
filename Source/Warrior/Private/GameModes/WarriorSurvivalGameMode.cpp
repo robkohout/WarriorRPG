c3753bdbfc3c476290cc7fd58bfd76bde3185fd6
robkohout <robkohout@hotmail.com>
2025-12-12 01:50:24 -0800

    Survival Mode Game Class

---
+++ Source/Warrior/Private/GameModes/WarriorSurvivalGameMode.cpp
@@ -0,0 +1,21 @@
+﻿// Rob Kohout All Rights Reserved
+
+
+#include "GameModes/WarriorSurvivalGameMode.h"
+
+void AWarriorSurvivalGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AWarriorSurvivalGameMode::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+}
+
+void AWarriorSurvivalGameMode::SetCurrentSurvivalGameModeState(EWarriorSurvivalGameModeState InState)
+{
+	CurrentSurvivalGameModeState = InState;
+	
+	OnSurvivalGameModeStateChanged.Broadcast(CurrentSurvivalGameModeState);
+}
---
+++ Source/Warrior/Public/GameModes/WarriorSurvivalGameMode.h
@@ -0,0 +1,42 @@
+﻿// Rob Kohout All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "WarriorBaseGameMode.h"
+#include "WarriorSurvivalGameMode.generated.h"
+
+UENUM(BlueprintType)
+enum class EWarriorSurvivalGameModeState : uint8
+{
+	WaitSpawnNewWave,
+	SpawningNewWave,
+	InProgress,
+	WaveCompleted,
+	AllWavesDone,
+	PlayerDied
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnSurvivalGameModeStateChanged, EWarriorSurvivalGameModeState, CurrentState);
+
+/**
+ * 
+ */
+UCLASS()
+class WARRIOR_API AWarriorSurvivalGameMode : public AWarriorBaseGameMode
+{
+	GENERATED_BODY()
+	
+protected:
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaSeconds) override;
+	
+private:
+	void SetCurrentSurvivalGameModeState(EWarriorSurvivalGameModeState InState);
+	
+	UPROPERTY()
+	EWarriorSurvivalGameModeState CurrentSurvivalGameModeState;
+	
+	UPROPERTY(BlueprintAssignable, BlueprintCallable)
+	FOnSurvivalGameModeStateChanged OnSurvivalGameModeStateChanged;	
+};
