d5b737d976861c9b61065a4db71593f2273c4d9f
robkohout <robkohout@hotmail.com>
2025-12-14 02:22:06 -0800

    Save Game Object

--- Source/Warrior/Private/WarriorGameplayTags.cpp
+++ Source/Warrior/Private/WarriorGameplayTags.cpp
@@ -99,5 +99,7 @@ namespace WarriorGameplayTags
 	// Game Data Tags
 	UE_DEFINE_GAMEPLAY_TAG(GameData_Level_SurvivalGameModeMap, "GameData.Level.SurvivalGameModeMap");
 	UE_DEFINE_GAMEPLAY_TAG(GameData_Level_MainMenuMap, "GameData.Level.MainMenuMap");
+	
+	UE_DEFINE_GAMEPLAY_TAG(GameData_SaveGame_Slot_1, "GameData.SaveGame.Slot.1");
 }
 
